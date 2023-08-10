72265ca11bbd2396c71c304ebc4e476bc6d7df4d
amit-khatke <141109542+amit-khatke@users.noreply.github.com>
2023-08-10 20:47:42 +0530

    2 files modified

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,12 @@ import 'zone.js/dist/zone';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { InstagramComponent } from './instagram/instagram.component';
 
 @Component({
   selector: 'my-app',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,InstagramComponent],
   template: `
     
   `,
